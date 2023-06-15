0311197c058a9e5271cf59c0b87e1302cde52e0c
Ernesto Pariona Díaz <eparionad@gmail.com>
2023-06-15 10:49:47 -0500

    add cach no-store services

--- app/institution/services/institution.services.js
+++ app/institution/services/institution.services.js
@@ -1,7 +1,9 @@
 export const getInstitutions = () => {
   const url = `http://127.0.0.1:1337/api/instituciones?populate=*`;
   const urlDeploy = `https://aidel-dev-app-c5t5b.ondigitalocean.app/api/instituciones?populate=deep`;
-  return fetch(urlDeploy)
+  return fetch(urlDeploy, {
+    cache: 'no-store',
+  })
     .then((response) => response.json())
     .then((data) => data.data);
 };
--- app/intervention/services/intervention.services.js
+++ app/intervention/services/intervention.services.js
@@ -1,7 +1,7 @@
 export const getInterventions = () => {
   const url = `http://127.0.0.1:1337/api/instituciones?populate=*`;
   const urlDeploy = `https://aidel-dev-app-c5t5b.ondigitalocean.app/api/intervenciones?populate=deep`;
-  return fetch(urlDeploy)
+  return fetch(urlDeploy, { cache: 'no-store' })
     .then((response) => response.json())
     .then((data) => data.data);
 };
