8c5d3452bbe521da7e9f8a085036c024f4b0f829
Ernesto Pariona Díaz <eparionad@gmail.com>
2023-06-15 17:26:00 -0500

    change maxlimit api1

--- app/intervention/services/intervention.services.js
+++ app/intervention/services/intervention.services.js
@@ -1,11 +1,7 @@
 export const getInterventions = () => {
   const url = `http://127.0.0.1:1337/api/instituciones?populate=*`;
-  const urlDeploy = `https://aidel-dev-app-c5t5b.ondigitalocean.app/api/intervenciones?populate=deep`;
-  return fetch(urlDeploy, {
-    next: {
-      revalidate: 60,
-    },
-  })
+  const urlDeploy = `https://aidel-dev-app-c5t5b.ondigitalocean.app/api/intervenciones?populate=deep&pagination[start]=0&pagination[limit]=100`;
+  return fetch(urlDeploy)
     .then((response) => response.json())
     .then((data) => data.data);
 };
